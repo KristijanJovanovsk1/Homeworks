0f65d5efd8613fe936ba4ae7ed8aa931bfd8e069
KristijanJovanovsk1 <kristijanjovanovski33@yahoo.com>
2021-04-22 15:33:08 +0200

    homework24

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP function</title>
+</head>
+<body>
+
+    <form action="post.php" method="POST">
+        <p>First name: <input type="text" name="firstname" value=""></p>
+        <p>Last name: <input type="text" name="lastname" value=""></p>
+        <input type="submit" name="submit" value="submit">
+    
+    </form>
+    
+</body>
+</html>
+
---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+
+  $name    =  $_POST['firstname'];
+  $lastname=   $_POST['lastname'];
+    echo "maybe im not complite failure "
+
+?>
